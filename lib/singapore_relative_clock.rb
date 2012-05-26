fb52b69b6cfcbf8c086a56449b6256381f9f6303
arrchana <arrchu@gmail.com>
2012-05-26 15:55:00 +0800

    object of GMTOffsetFinder added

--- lib/singapore_relative_clock.rb
+++ lib/singapore_relative_clock.rb
@@ -1,10 +1,11 @@
 require_relative 'gmt_offset_finder'
 
 class SingaporeRelativeClock
+	@finder = GMTOffsetFinder.new
 
 	def timeDifferenceTo(city)
 		sgTime = Time.new + 8
-		cityTime = offsetFor(city)
+		cityTime = @finder.offsetFor(city)
 		sgTime - cityTime
 		
 	end
