5c2b84507c975c34b54cf38640ac3c724ea7ac7a
arrchana <arrchu@gmail.com>
2012-05-26 15:29:13 +0800

    just testing for travis

--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -5,7 +5,7 @@ describe SingaporeRelativeClock do
 		@clock = SingaporeRelativeClock.new
 	end
 
-	it "should return -2 for sydney" do
+	it "should return -2 for sydney!" do
 		@clock.timeDifferenceTo("sydney").should == -2
 	end
 
